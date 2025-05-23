db4c3f8ec889d927dfcb08a7dc69ad50a3c45c91
RodrigoRodriguez1 <rodrigorfig1@gmail.com>
2025-05-22 22:03:25 -0300

    Arrumando vestigios de conflitos

--- src/Adapters/Driven/StackFood.Infra/Persistence/Repositories/OrderRepository.cs
+++ src/Adapters/Driven/StackFood.Infra/Persistence/Repositories/OrderRepository.cs
@@ -14,23 +14,19 @@ namespace StackFood.Infra.Persistence.Repositories
         }
 
         public async Task CreateAsync(Order order)
-        public void CreateAsync(Order order)
         {
             await _context.Orders.AddAsync(order);
             await _context.SaveChangesAsync();
-            _context.Orders.Add(order);
         }
 
         public async Task<List<Order>> GetAllAsync()
         {
             return await _context.Orders.Include(o => o.Products).ToListAsync();
-            return await _context.Orders.ToListAsync();
         }
 
         public async Task<Order> GetByIdAsync(Guid id)
         {
             return await _context.Orders.Include(o => o.Products).FirstOrDefaultAsync(o => o.Id == id);
-            return await _context.Orders.FirstOrDefaultAsync(o => o.Id == id);
         }
 
         public async Task SaveAsync()

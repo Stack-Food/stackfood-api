1f3767764a949279b32ef39f05b0dbe6ec54b031
LuizFelipeAlvesMaia <luiz.felipeam@hotmail.com>
2025-05-26 19:39:52 -0300

    Feat: Add database migration on application startup

--- src/Adapters/Driving/StackFood.API/Program.cs
+++ src/Adapters/Driving/StackFood.API/Program.cs
@@ -28,6 +28,12 @@ builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {

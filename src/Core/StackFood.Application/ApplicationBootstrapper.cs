d6f149d25957270ff1b2b5a7d1d0cd7e165a7b05
Leonardo Duarte <leo.duarte.dev@gmail.com>
2025-05-29 22:53:01 -0300

    fix: ajuste injeçao de dependencia

--- src/Core/StackFood.Application/ApplicationBootstrapper.cs
+++ src/Core/StackFood.Application/ApplicationBootstrapper.cs
@@ -1,6 +1,4 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
-using StackFood.Application.Interfaces.Services;
-using StackFood.Application.Services;
 using StackFood.Application.UseCases.Customers.Create;
 using StackFood.Application.UseCases.Customers.GetByCpf;
 using StackFood.Application.UseCases.Orders.ChangeStatus;
@@ -9,6 +7,11 @@ using StackFood.Application.UseCases.Orders.GetAll;
 using StackFood.Application.UseCases.Orders.GetById;
 using StackFood.Application.UseCases.Orders.Payments.Check;
 using StackFood.Application.UseCases.Orders.Payments.Generate;
+using StackFood.Application.UseCases.Products.Create;
+using StackFood.Application.UseCases.Products.Delete;
+using StackFood.Application.UseCases.Products.GetAll;
+using StackFood.Application.UseCases.Products.GetById;
+using StackFood.Application.UseCases.Products.Update;
 
 namespace StackFood.Application
 {
@@ -23,8 +26,12 @@ namespace StackFood.Application
             services.AddScoped<ICheckPaymentUseCase, CheckPaymentUseCase>();
             services.AddScoped<IChangeStatusOrderUseCase, ChangeStatusOrderUseCase>();
 
-            services.AddScoped<IProductService, ProductService>();
-
+            services.AddScoped<ICreateProductUseCase, CreateProductUseCase>();
+            services.AddScoped<IDeleteProductUseCase, DeleteProductUseCase>();
+            services.AddScoped<IGetAllProductUseCase, GetAllProductUseCase>();
+            services.AddScoped<IGetByIdProductUseCase, GetByIdProductUseCase>();
+            services.AddScoped<IUpdateProductUseCase, UpdateProductUseCase>();
+            
             services.AddScoped<ICreateCustomerUseCase, CreateCustomerUseCase>();
             services.AddScoped<IGetByCpfCustomerUseCase, GetByCpfCustomerUseCase>();
         }
